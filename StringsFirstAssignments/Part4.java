d3f01c91e7b11a41b0363f8427d97ed42d557b29
anis25 <aniscseru6125@gmail.com>
2017-09-12 00:38:23 +0600

    method to read an URL Resource

---
+++ StringsFirstAssignments/Part4.java
@@ -0,0 +1,12 @@
+
+import edu.duke.*;
+
+public class Part4
+{
+    public void youtubeCheck()
+    {
+        URLResource res = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+        
+        
+    }
+}
